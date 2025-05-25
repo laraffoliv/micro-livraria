221bdc8f67e237bc258bf1fa426b4123d68601c0
Lara de Oliveira <laraffolivera@gmail.com>
2025-05-25 17:23:38 -0300

    Tarefa prática #1 - Microservices

--- services/inventory/index.js
+++ services/inventory/index.js
@@ -22,7 +22,13 @@ server.addService(inventoryProto.InventoryService.service, {
     },
 });
 
-server.bindAsync('127.0.0.1:3002', grpc.ServerCredentials.createInsecure(), () => {
-    console.log('Inventory Service running at http://127.0.0.1:3002');
-    server.start();
-});
+SearchProductByID: (payload, callback) => {
+    callback(
+        null,
+        products.find((product) => product.id == payload.request.id)
+    );
+},
+    server.bindAsync('127.0.0.1:3002', grpc.ServerCredentials.createInsecure(), () => {
+        console.log('Inventory Service running at http://127.0.0.1:3002');
+        server.start();
+    });
